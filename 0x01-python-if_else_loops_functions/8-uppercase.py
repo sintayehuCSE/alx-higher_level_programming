2dbeac61f3c0e398243b7fc24848256c82559907
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-01-17 21:25:11 +0300

    A python script that print string in uppercase character

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for char in str:
+        char = ord(char)
+        if char >= 97 and char <= 122:
+            char = char - 32
+        print("{:c}".format(char), end="")
+    print()
