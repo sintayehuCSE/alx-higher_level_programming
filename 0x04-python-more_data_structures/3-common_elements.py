0e84ace8fc22ee784d791e37ad2a7bc2feb6570c
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-01-27 12:41:34 +0300

    A python script that returns an intersection of two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -6,7 +6,3 @@ def common_elements(set_1, set_2):
             if first == second:
                 new_set.add(first)
     return new_set
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
