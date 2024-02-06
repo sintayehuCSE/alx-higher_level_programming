3a6b3328160f896e24a611aa40e7e2f20f2a9f52
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-02-06 04:30:46 +0300

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb_print = 0;
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            nb_print += 1
+        except IndexError:
+            raise
+        except Exception:
+            continue
+    print()
+    return (nb_print)
