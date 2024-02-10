a80961a72f4d133299afb167e9f13b15a2abc751
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-02-10 12:08:54 +0300

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/user/bin/python3
+"""
+This module named 0-square is a module for defining an empty
+class named Square
+"""
 class Square:
+    """ This is a syntax for defining a class in python """
     pass
