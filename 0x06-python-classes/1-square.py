4f1451da58e42e081fdecb13b901939039ff3ad2
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-02-10 13:22:06 +0300

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,8 @@ intialize a private attribute of an object
 class Square:
     """Defines a private instance attribute"""
     def __init__(self, size):
-        """Initialize the size attribute of an instance"""
+        """Initialize the size attribute of an instance
+        Args:
+            size (int): The size of the new square
+        """
         self.__size = size
