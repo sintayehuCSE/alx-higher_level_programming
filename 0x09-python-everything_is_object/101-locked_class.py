1dae681bc7018d37f47709c574411e787f06554e
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-03 21:19:35 +0300

    Prevent dynamic creation of instance attribute by user

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a Low memory cost class."""
+
+
+
+class LockedClass:
+    """Defines a LockeClass that limit an attribute of its instance to one."""
+    __slots__ = ["first_name"]
