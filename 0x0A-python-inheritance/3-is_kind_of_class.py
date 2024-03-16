54c0eef4f8e18fe721f434951cce6ab28f409836
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-16 11:13:10 +0300

    Check if object is instance of a class or its parents

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a function that determine the class to which an
+   Instance belongs.
+"""
+
+def is_kind_of_class(obj, a_class):
+    """Check if object is an instance of a class or its parents.
+    Args:
+        obj (object): The object
+        a_class (class): The class
+    Return:
+        True: If obj is instance of a_class
+        False: If obj is not instance of a_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
