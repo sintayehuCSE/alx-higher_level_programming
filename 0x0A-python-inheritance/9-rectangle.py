a32e87aee767575ae97dfac130a71403d1116233
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-17 11:23:18 +0300

    Full Rectanglr

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -13,3 +13,13 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Get the area of a Rectangle."""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """The unofficial printable string representation
+           of Rectangle object.
+        """
+        return ('[Rectangle] ' + str(self.__width) + '/' + str(self.__height))
