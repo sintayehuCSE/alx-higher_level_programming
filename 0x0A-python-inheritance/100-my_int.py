111c297f8c00c64cfc08e9c9c62876b5c17a5b2e
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-17 12:50:37 +0300

    Custom integer class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,8 @@ class MyInt(int):
     """The custom integer class."""
     def __eq__(self, other):
         """Compare self with other"""
-        return (not(int.__eq__(self, other)))
+        return (not (int.__eq__(self, other)))
+
     def __ne__(self, other):
         """The not equal comparison."""
-        return (not(int.__ne__(self, other)))
+        return (not (int.__ne__(self, other)))
