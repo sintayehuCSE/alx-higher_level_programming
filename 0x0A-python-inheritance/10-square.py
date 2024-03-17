9a974d6b4a204faeb31c22c72155980f3c61f67a
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-17 12:01:15 +0300

    The Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,4 +9,5 @@ class Square(Rectangle):
     """The Square class."""
     def __init__(self, size):
         """Initialize the Square."""
+        self.integer_validator("size", size)
         Rectangle.__init__(self, size, size)
