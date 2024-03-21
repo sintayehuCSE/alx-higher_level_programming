1fbbb0f9c716e895cd2e64515da7dabae02fd879
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-21 17:03:16 +0300

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a class for maniipulating its instances's dictionary
+   representation as a JSON string.
+"""
+
+
+class Student():
+    """Define a Student class."""
+    def __init__(self, first_name, last_name, age):
+        """Instantiate class instances."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary repreesentation of Student's instance."""
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            _dict_ = {}
+            for i in range(len(attrs)):
+                if hasattr(self, attrs[i]):
+                    _dict_[attrs[i]] = self.__dict__[attrs[i]]
+            return (_dict_)
+
+    def reload_from_json(self, json):
+        """Set all attribute of an instance to a new dictionary."""
+        self.__dict__ = json
