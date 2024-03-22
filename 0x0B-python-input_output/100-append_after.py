7baf7530d8101b5529191a3b5e542b8ec59bbfab
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-23 00:22:59 +0300

    Search and Update a text of line with new string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,6 +5,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Insert a new line text in to a file."""
     with open(filename, "r", encoding="utf-8") as f:
         line = f.readlines()
     with open(filename, "w", encoding="utf-8") as f2:
