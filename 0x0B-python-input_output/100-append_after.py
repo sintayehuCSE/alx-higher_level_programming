1316bc90de1a406df72173e916318e732e447a27
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-23 00:17:13 +0300

    Search and Update a text of line with new string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a function that search for substring on a file line and
+   Enter a new line in for each occurence of the substring.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r", encoding="utf-8") as f:
+        line = f.readlines()
+    with open(filename, "w", encoding="utf-8") as f2:
+        for item in line:
+            if search_string in item:
+                f2.write(item + new_string)
+            else:
+                f2.write(item)
