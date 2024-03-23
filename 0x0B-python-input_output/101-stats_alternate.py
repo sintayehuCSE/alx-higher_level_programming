be322b9bb2aa5c1be768313ace1a697c6381e6b3
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-23 12:42:51 +0300

    Log parsing

---
+++ 0x0B-python-input_output/101-stats_alternate.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A script that reads `stdin` line by line and computes metrics"""
+
+
+import sys
+
+
+line_content = []
+code = [200, 301, 400, 401, 403, 404, 405, 500]
+found = []
+line_no = 0
+file_size = 0
+try:
+    while (1):
+        line_content.append(sys.stdin.readline())
+        line_no += 1
+        if line_no == 10:
+            for item in line_content:
+                file_size += len(item)
+                for cd in code:
+                    if str(cd) in item:
+                        found.append(cd)
+            found.sort()
+            line_content.clear()
+            line_content = []
+            line_no = 0
+            print("File size: {:d}".format(file_size))
+            [print(item, ":") for item in found]
+            found.clear()
+            found = []
+except KeyboardInterrupt as e:
+    if line_content:
+        for item in line_content:
+            file_size += len(item)
+            for cd in code:
+                if str(cd) in item:
+                    found.append(cd)
+        found.sort()
+        print("File size: {:d}".format(file_size))
+        [print(item, ":") for item in found]
+    raise e
