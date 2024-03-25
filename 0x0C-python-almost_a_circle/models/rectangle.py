170c68912b493c3ebeacbd0ef67fc795b637f7fa
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-25 16:51:38 +0300

    Validator of attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,7 +75,7 @@ class Rectangle(Base):
     def y(self, value):
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        elif value > 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
