290d5f104107fc5ef06e2989b3e6d1bca01953bf
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-27 09:05:26 +0300

    The square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines the special type of rectangle - Square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Define the Square class."""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the instance of a Square.
+           Args:
+               size (int): The size of the square.
+               x (int): The 'x' coordinate of the square instance.
+               y (int): The 'y' coordinate of the square instance.
+               id (int): The id of the Square instance.
+        """
+        super().__init__(size, size, x, y, id)
+        print(self.__dict__)
+
+    def __str__(self):
+        """Return nice printable string representation of the string object"""
+        return "[Square] {} {}/{} - {}".format(self.id, self.x, self.y,
+                                               self.width)
