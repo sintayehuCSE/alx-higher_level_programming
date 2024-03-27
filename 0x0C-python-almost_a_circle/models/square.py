89c3a16f100be543bafd689aae6e1e2a867476e4
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-27 09:33:44 +0300

    Proper overloading of __str__

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,4 +18,4 @@ class Square(Rectangle):
     def __str__(self):
         """Return nice printable string representation of the string object"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                               self.width)
+                                                 self.width)
