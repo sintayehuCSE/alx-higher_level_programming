8fedba1483ae8dddb2a4646608d0beec5d77cab7
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-06-09 14:04:26 +0300

    Drop DATABASE if it exist

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
 -- A script that deletes the database hbtn_0c_o from MYSQL Server
    -- If the database hbtn_0c_0 doesn’t exist, your script should not fail
    -- You are not allowed to use the SELECT or SHOW statements
-DROP SCHEMA IF EXISTS hbtn_0c_o;
+DROP SCHEMA IF EXISTS hbtn_0c_0;
