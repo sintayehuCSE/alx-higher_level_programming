83223081056c8fae944757e5feeaf734aae29039
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-06-09 13:40:27 +0300

    Create a database whatever so

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that create a database hbtn_0c_0 in MYSQL server
+   -- If the database hbtn_0c_0 already exists, your script should not fail
+   -- You are not allowed to use the SELECT or SHOW statements
+CREATE SCHEMA IF NOT EXISTS hbtn_0c_0;
