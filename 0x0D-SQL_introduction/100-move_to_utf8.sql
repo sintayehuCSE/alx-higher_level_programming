d1d18f609282dc3fb69eea5de313d22fadec1e89
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-06-09 21:41:06 +0300

    Alter table and Dtabase information

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Convert Character SET and COLLATE
-ALTER DATABASE CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
