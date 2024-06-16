f2b9e576d734a8bc17b2920ef2a12195e7af5601
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-06-16 16:55:38 +0300

    Database dump

---
+++ 0x0E-SQL_more_queries/hbtn_0d_tvshows.sql
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>NB60BCRJ02DDZG4A</RequestId><HostId>w+Y6NLBHnbM0XUoao8uHVzggaLs55xcLdYxadY36OPlVJZ5RQYXLgh0WcSI9qDw+chCOTB2XH1M=</HostId></Error>
