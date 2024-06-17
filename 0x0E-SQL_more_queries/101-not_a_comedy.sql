f8053c8671674619535514c292fa2449d1e5d11b
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-06-17 15:58:57 +0300

    No Comedy ToNight

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,10 @@
+-- No Comedy ToNight
+SELECT DISTINCT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.id NOT IN
+(SELECT tv_show_genres.show_id
+FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title ASC;
