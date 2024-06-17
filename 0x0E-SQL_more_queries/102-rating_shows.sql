39a2fa1410053f0eb81a41330b0daab17592c0f0
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-06-17 16:41:21 +0300

    JOIN and SUM

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- A script that lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, SUM(rate) as rating
+FROM tv_shows
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
