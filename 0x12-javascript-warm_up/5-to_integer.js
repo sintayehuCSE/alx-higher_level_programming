1d5df6af8b7b9e14664d22cb33bc3e390584aa34
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-07-19 17:48:23 +0300

    Number class, its static method and properties/variables, instance method

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let num = Number(process.argv[2]);
+const num = Number(process.argv[2]);
 if (Number.isNaN(num)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log(`My number: ${num}`);
+  console.log(`My number: ${num}`);
 }
