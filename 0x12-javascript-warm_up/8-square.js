5e26432d89618b19345027dfee6052810999e1cc
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-07-20 13:28:29 +0300

    Print Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = Number(process.argv[2]);
+if (!size) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
