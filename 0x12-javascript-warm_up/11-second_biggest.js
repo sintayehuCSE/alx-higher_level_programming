74b3398928466ef9e46bb49606237898209337a0
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-07-21 12:12:14 +0300

    Second biggest element of an array from a command line input

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+let arr = Number(process.argv);
+arr = arr.filter((n) => !isNaN(n));
+const len = arr.length;
+if (len === 0 || len === 1) {
+  console.log(1);
+} else {
+  let secBig;
+  let count = 0;
+  let iter = 0;
+  while (iter < len) {
+    secBig = arr[iter];
+    for (let i = 0; i < len; i++) {
+      if (arr[i] > secBig) count++;
+      if (count > 1) break;
+    }
+    if (count === 1) {
+      console.log(secBig);
+      break;
+    }
+    count = 0;
+    iter++;
+  }
+}
