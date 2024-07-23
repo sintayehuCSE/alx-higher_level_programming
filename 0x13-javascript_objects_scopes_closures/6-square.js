bd30873e0063e98a32c29f1415b17640c6d5f2a3
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-07-23 22:17:04 +0300

    Inheriting from GRANPA or SuperParent in the hierarchy tree

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,11 @@ module.exports = class Square extends Square5 {
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
-	let side = '';
-	for (let j = 0; j < this.width; j++) {
-	  side += 'C'
-	}
-	console.log(side);
+        let side = '';
+        for (let j = 0; j < this.width; j++) {
+	  side += c;
+        }
+        console.log(side);
       }
     } else {
       this.print();
