c611fdd6230de77bcb0bea18a58da39ffba42df2
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-07-23 11:44:06 +0300

    Instance method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,13 +6,14 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
-	row += 'X';
+        row += 'X';
       }
       console.log(row);
     }
   }
-}
+};
