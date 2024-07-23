ac17eb455e59d37ee94a504987f3930d10b0e7cf
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-07-23 22:03:46 +0300

    Inheriting from GRANPA or SuperParent in the hierarchy tree

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Square5 = require('./5-square');
+module.exports = class Square extends Square5 {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+	let side = '';
+	for (let j = 0; j < this.width; j++) {
+	  side += 'C'
+	}
+	console.log(side);
+      }
+    } else {
+      this.print();
+    }
+  }
+};
