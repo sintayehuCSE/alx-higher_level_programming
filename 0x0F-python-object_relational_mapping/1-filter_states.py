3abf5ebc23faa407f0e4a6c390b5a02b3b8e16b9
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-08 18:11:20 +0300

    Filter States with LIKE operator

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that lists all STATES with a name starting with N (upper N)"""
+"""  A script that lists all STATES with a name starting with N (upper N) """
 import MySQLdb
 from sys import argv
 
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    qry = """SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY
+    qry = """SELECT * FROM states WHERE states.name LIKE BINARY 'N%' ORDER BY
     states.id ASC"""
     cur.execute(qry)
     res = cur.fetchall()
