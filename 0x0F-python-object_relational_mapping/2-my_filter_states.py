b562da93b249e44b77c2a2b2a93499e8ea39d37d
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-08 18:44:33 +0300

    Filter state by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" A script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument """
+"""  A script that lists all STATES with a name starting with N (upper N) """
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="127.0.0.1", port=3306, user=argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name={}".format(argv[4]))
+    qry = """SELECT * FROM states WHERE states.name LIKE BINARY '{}' ORDER BY
+    states.id ASC""".format(argv[4])
+    cur.execute(qry)
     res = cur.fetchall()
     for row in res:
         print(row)
