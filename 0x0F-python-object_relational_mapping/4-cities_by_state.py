24bc5f827dbf7d5fdd2a98add69612b4b03c9312
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-08 20:11:25 +0300

    Inner Join with MySQLdb

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  A script that lists all STATES with a name starting with N (upper N) """
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM states INNER
+    JOIN cities ON cities.state_id = states.id ORDER BY cities.id""")
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+    cur.close()
+    conn.close()
