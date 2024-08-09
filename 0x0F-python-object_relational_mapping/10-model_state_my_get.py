cd17e019188bcad128cae104647024b0bc66b030
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-09 16:20:28 +0300

    Get a specific state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that fetch one state from a DB using SQLAlchemy
+"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from model_state import State
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    qry = session.query(State).filter(State.name.like('%{}%'
+                                                      .format(argv[4]))).all()
+    if qry:
+        for row in qry:
+            print(row.id)
+    else:
+        print('Not found')
+    session.close()
