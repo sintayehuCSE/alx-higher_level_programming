3d026959dc0ed55a4f8066ac52b1c9c6021eb92d
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-09 12:00:55 +0300

    State Model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Articulate definition of class State using
 declarative system of SQLAlchemy ORM."""
-from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import declarative_base
-from sys import argv
 
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                       .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Class with id and name attribute of each State
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
