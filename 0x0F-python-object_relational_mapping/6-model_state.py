1ed8bebdac5f33c6661825ec7810aca1d21b6923
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-09 12:00:08 +0300

    Deleted

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""
-Starts a linink of declarative class to table in database
-"""
-if __name__ == "__main__":
-    from model_state import engine, Base
-    Base.metadata.create_all(engine)
