747c74af53f4f1c8e2e556798f24679eb1f5a9e0
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-09 16:23:57 +0300

    Get a specific state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+                           pool_pre_ping=True, echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    qry = session.query(State).filter(State.name.like('%{}%'
-                                                      .format(argv[4]))).all()
+    qry = session.query(State).filter(State.name.like('{}'
+                                                      .format(argv[4]))).one_or_none()
     if qry:
-        for row in qry:
-            print(row.id)
+        #for row in qry:
+            print(qry.id)
     else:
         print('Not found')
     session.close()
