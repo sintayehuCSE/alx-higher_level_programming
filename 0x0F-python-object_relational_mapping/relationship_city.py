48b7df297fe57c27775713573d9cfb8d8e49a539
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-10 23:27:08 +0300

    Cities relationship to State

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Declares a class that performs mapping to table in DB
+"""
+from model_state import Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    Maps the City class to table cities in DB
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship('State', back_populates='cities')
