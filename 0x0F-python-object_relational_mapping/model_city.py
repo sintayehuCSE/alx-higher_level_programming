3a2357784a26ddbde62c48d3d1eeadd01707bc8d
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-10 15:19:59 +0300

    City Model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-Declares and mapps the City class to its table in DB
+Declares a class that performs mapping to table in DB
 """
 from model_state import Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 
 
 class City(Base):
+    """
+    Maps the City class to table cities in DB
+    """
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
