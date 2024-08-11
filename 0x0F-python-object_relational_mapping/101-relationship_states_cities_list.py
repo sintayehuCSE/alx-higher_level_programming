d4853b8804e79d1a00cbc628cdf65024b4858d5e
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-08-11 13:18:20 +0300

    Access State object their cities via configured relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+List State object and their cities using configured relationship
+"""
+if __name__ == '__main__':
+    from relationship_state import State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    qry = session.query(State).join(City).order_by(State.id, City.id).all()
+    for state in qry:
+        print(state.id, state.name, sep=': ')
+        for city in state.cities:
+            print('\t', end='')
+            print(city.id, city.name, sep=': ')
+
+    session.close()
