79c63144b017197ab894a77055a99d02fc4f3542
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-04 11:56:47 +0300

    Data from JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST method with a JSON file as data content to the server
-curl -sH Content-Type: application/json -d "$(cat $2)" "$1"
+curl -sH "Content-Type: application/json" -d "$(cat $2)" "$1"
