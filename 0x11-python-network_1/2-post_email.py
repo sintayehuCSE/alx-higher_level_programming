7cb36f38fa1a8bfd319af4920c78ce47e15be6c0
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-06 10:20:16 +0300

    POST a parameter to the URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     data = {'email': argv[2]}
-    data = request.parse.urlencode(data).encode('ascii')
+    data = parse.urlencode(data).encode('ascii')
     req = request.Request(argv[1], data)
     with request.urlopen(req) as res:
         body = res.read()
