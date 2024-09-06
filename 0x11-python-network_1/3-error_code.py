7f03703ac0b6608dbb8351555ce5e86f27b00774
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-06 10:27:57 +0300

    Handle HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that make URL request while handling exceptions"""
+from sys import argv
+from urllib import request
+
+
+if __name__ == "__main__":
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
