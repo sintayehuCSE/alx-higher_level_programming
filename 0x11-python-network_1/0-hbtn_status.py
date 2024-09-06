b8119db8e57e8d0c4c8b16ef69a44885305c2714
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-06 09:34:37 +0300

    What is My Status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# A Python script that fetches a resource from the internet
+from urllib import request
+
+
+if __name__ == "__main__":
+    req = request.Request("https://alx-intranet.hbtn.io/status")
+    with request.urlopen(req) as req:
+        body = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
