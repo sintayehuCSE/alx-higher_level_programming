9d751d5f2e28f0dda9b2d51aa74fcf9429904e14
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-09-06 10:18:40 +0300

    POST a parameter to the URL

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""POST an email to a URL"""
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = {'email': argv[2]}
+    data = request.parse.urlencode(data).encode('ascii')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as res:
+        body = res.read()
+        print(body.decode('utf-8'))
